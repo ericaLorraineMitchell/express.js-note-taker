8597ef9917492d0568468646991106dc7cadfeb2
Lorraine Mitchell <elorrainemitchell@gmail.com>
2022-03-18 10:48:48 -0400

    work on getting new notes to append on html as well

--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,20 @@ const res = require("express/lib/response");
 router.get("/", (req, res) => res.send("json data"));
 router.get("/notes", (req, res) => res.json(notesData));
 
-//Post fetch new note, add to json storage
+//Post fetch new note with unique id, show and add to json storage
 router.post("/notes", (req, res) => {
   const newNote = req.body;
   fs.appendFile(
     path.join(__dirname, "../db/db.json"),
     JSON.stringify(newNote),
+    function (err, data) {
+      res.json(data);
+    }
+  );
+  const showNote = req.body;
+  fs.appendFile(
+    path.join(__dirname, "/notes"),
+    JSON.stringify(showNote),
     function (err, data) {
       res.json(data);
     }
